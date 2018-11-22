526401a5f3f53ce59aa7a6f237153dba3c37d406
EX-HUPENG <EX-HUPENG003@pingan.com.cn>
2018-11-22 11:09:12 +0800

    Initial Commit of Library
    Signed-off-by: EX-HUPENG <EX-HUPENG003@pingan.com.cn>

---
+++ Example/Pods/Target Support Files/testing1/testing1-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_testing1 : NSObject
+@end
+@implementation PodsDummy_testing1
+@end
---
+++ Example/Pods/Target Support Files/testing1/testing1-umbrella.h
@@ -0,0 +1,21 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "NSString+Chinese.h"
+#import "NSString+SCString.h"
+#import "UIColor+HexColor.h"
+#import "UITableViewCell+cell.h"
+#import "UITextView+SCtextView.h"
+
+FOUNDATION_EXPORT double testing1VersionNumber;
+FOUNDATION_EXPORT const unsigned char testing1VersionString[];
+
